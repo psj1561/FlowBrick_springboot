2475b702b436acc89de88876300b9390bb8434fd
Park SeongJung <31840373+psj1561@users.noreply.github.com>
2024-01-30 22:38:26 +0900

    change

--- src/main/java/com/web/spring/controller/WebController.java
+++ src/main/java/com/web/spring/controller/WebController.java
@@ -9,7 +9,7 @@ public class WebController {
 
 	
 	// http://211.63.89.67:2222/pms/index.do	
-	// http://localhost:2222/index.do
+	// http://localhost:2223/indpsjex.do
 	@RequestMapping("index.do")
 	public String index() {
 		return "index";
